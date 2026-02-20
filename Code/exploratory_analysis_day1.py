7db4ffc1498efa88254d7be66f00cfdd76a9b30d
kmcole06 <kaitlynmckennacole@gmail.com>
2026-02-20 12:58:25 -0500

    Homework 1 Graph Code + Notebook Update

--- Code/exploratory_analysis_day1.py
+++ Code/exploratory_analysis_day1.py
@@ -4,7 +4,19 @@ import matplotlib.pyplot as plt
 
 #%%
 # Load the data
-data = pd.read_csv('../Data/mystery_virus_daily_active_counts_RELEASE#1.csv', parse_dates=['date'], header=0, index_col=None)
+data = pd.read_csv('/Users\yqr8pz\Documents\BME 2315\Module-2-Epidemics-SIR-Modeling\Data\mystery_virus_daily_active_counts_RELEASE#1.csv', parse_dates=['date'], header=0, index_col=None)
 
 #%%
 # Make a plot of the active cases over time
+
+# Create a day number column (days since first date)
+data['day'] = (data['date'] - data['date'].min()).dt.days
+
+# Plot
+plt.figure()
+plt.plot(data['day'], data['active reported daily cases'])
+plt.xlabel('Day')
+plt.ylabel('Active Infections')
+plt.title('Active Infections vs Day (Data Release #1)')
+plt.show()
+# %%
